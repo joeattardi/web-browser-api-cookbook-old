7b63deeab89190de79648c80a51b12391ad43216
Joe Attardi <jattardi@gmail.com>
2023-03-15 23:38:39 -0400

    Add code rendering in examples

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,15 @@
 const navigation = require('@11ty/eleventy-navigation');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const webc = require('@11ty/eleventy-plugin-webc');
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPlugin(navigation);
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(webc, {
+    components: 'src/_includes/_components/**/*.webc'
+  });
   eleventyConfig.addPassthroughCopy('src/assets/images');
   return {
     dir: {
