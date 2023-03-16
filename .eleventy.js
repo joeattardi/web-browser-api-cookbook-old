0428eb756b733f705b015f6e5f044e948ea824b9
Joe Attardi <jattardi@gmail.com>
2023-03-15 22:44:33 -0400

    More changes for side nav

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,14 @@
-const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const navigation = require('@11ty/eleventy-navigation');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(navigation);
+  eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy('src/assets/images');
   return {
     dir: {
       input: 'src',
-      output: 'public'
-    }
-  }
-}
+      output: 'public',
+    },
+  };
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['src/**/*.html', 'src/**/*.njk'],
+  content: ['src/**/*.html', 'src/**/*.njk', 'src/**/*.js'],
   theme: {
     extend: {},
   },
