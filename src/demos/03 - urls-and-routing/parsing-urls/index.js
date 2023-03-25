247ec72f3c361e8d37229fb348b87b655f4a1f19
Joe Attardi <jattardi@gmail.com>
2023-03-24 22:02:46 -0400

    data experiments WIP

---
+++ src/demos/03 - urls-and-routing/parsing-urls/index.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('form');
+const results = document.querySelector('#results');
+
+// The properties of the URL we want to show.
+const urlProperties = [
+  'host', 'hostname', 'href',
+  'origin', 'pathname', 'protocol',
+  'search'
+];
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const url = new URL(event.target.elements.url.value);
+  event.target.elements.url.value = '';
+
+  const card = document.querySelector('#url-card').content.cloneNode(true).firstElementChild;
+  const urlDetails = card.querySelector('ul');
+  card.querySelector('.url').textContent = url.toString();
+
+  let details = '';
+  urlProperties
+    .filter(property => url[property].length)
+    .forEach(property => {
+      details += `
+        <li><strong>${property}</strong>: ${url[property]}</li>
+      `;
+    });
+    urlDetails.innerHTML = details;
+
+  results.insertBefore(card, results.firstElementChild);
+});
